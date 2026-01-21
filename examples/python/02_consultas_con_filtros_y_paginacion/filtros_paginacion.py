36380d5181f4a4d97694f7aa830531f5b743cd79
Samuel <sacosta@saintnet.com>
2026-01-21 14:04:28 -0400

    Nuevos ejemplos

---
+++ examples/python/02_consultas_con_filtros_y_paginacion/filtros_paginacion.py
@@ -0,0 +1,63 @@
+import requests
+import base64
+import json
+
+API_URL = 'http://64.135.37.214:8080/api/v1'
+API_KEY = 'B5D31933-C996-476C-B116-EF212A41479A'
+API_ID = '1093'
+USER = '001'
+PASSWORD = '12345'
+
+def iniciar_sesion():
+    endpoint = API_URL + '/main/login'
+    auth_str = f'{USER}:{PASSWORD}'
+    auth =  base64.b64encode(auth_str.encode()).decode()
+
+    headers = {
+        'x-api-key': API_KEY,
+        'x-api-id': API_ID,
+        'Authorization': f'Basic {auth}'
+    }
+
+    body = {
+        'terminal': 'python'
+    }
+
+    try:
+        response = requests.post(endpoint, headers=headers, json=body)
+        response.raise_for_status()
+
+        session_token = response.headers.get('Pragma')
+
+        if session_token:
+            return session_token
+        else:
+            return None
+    except requests.exceptions.RequestException as e:
+        return e
+    
+def consultar_clientes():
+    token = iniciar_sesion()
+
+    if not token: return 'Error'
+
+    filtros = 'saldo>2000'
+    limite = 5
+    endpoint = f'{API_URL}/adm/customers?{filtros}&limit={limite}'
+
+    headers = {
+        'Pragma': token
+    }
+
+    try:
+        response = requests.get(endpoint, headers=headers)
+        response.raise_for_status()
+        clientes = response.json()
+
+        for cliente in clientes:
+            print(f'Codigo: {cliente.get("codclie")} | Nombre: {cliente.get("descrip")} | Saldo: {cliente.get("saldo")}')
+    except requests.exceptions.RequestException as e:
+        return f'fallo en la consulta {e}'
+    
+if __name__ == "__main__":
+    consultar_clientes()
